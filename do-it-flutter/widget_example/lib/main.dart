3bd409d2fa984d7e299bf9f3d09a6cf8ad28a8fb
masterhyuns <masterhyuns@naver.com>
2021-05-10 11:56:38 +0900

    widget_example

--- do-it-flutter/widget_example/lib/main.dart
+++ do-it-flutter/widget_example/lib/main.dart
@@ -5,109 +5,98 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static const String _title = "Widget Example";
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(title: _title, home: WidgetApp());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class WidgetApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _WidgetApp createState() => _WidgetApp();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WidgetApp extends State<WidgetApp> {
+  List _buttonList = ['더하기','빼기','곱하기','나누기'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems = [];
+  String _buttonText = '';
+  String sum = '';
+  TextEditingController value1 = TextEditingController();
+  TextEditingController value2 = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+  @override
+  void initState() {
+    super.initState();
+    for(var item in _buttonList){
+      _dropDownMenuItems.add(DropdownMenuItem(value:item, child: Text(item)));
+    }
+    _buttonText = _dropDownMenuItems[0].value!;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(title: Text('Widget Example')),
+        body: Container(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Text('결과 : $sum', style: TextStyle(fontSize: 20),),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: value1,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: value2,
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.all(15),
+                    child: ElevatedButton.icon(
+                        icon: Icon(Icons.add, size: 18),
+                        label: Text(_buttonText),
+                        onPressed: () {
+                          setState(() {
+
+                            var value1Int = int.parse(value1.value.text);
+                            var value2Int = int.parse(value2.value.text);
+                            var result ;
+                            if(_buttonText == '더하기'){
+                              result = value1Int + value2Int;
+                            }else if(_buttonText == '빼기'){
+                              result = value1Int - value2Int;
+                            }else if(_buttonText == '곱하기'){
+                              result = value1Int * value2Int;
+                            }else{
+                              result = value1Int / value2Int;
+                            }
+                            sum = '$result';
+                          });
+                        }))
+                ,
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: DropdownButton(items:_dropDownMenuItems, onChanged: (value){
+                    setState(() {
+                      _buttonText = '$value';
+                    });
+                  }, value: _buttonText),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        ));
   }
 }
